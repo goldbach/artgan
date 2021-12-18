e2c44f683a31b4dfed2218e94ebd5853056bb019
Jakob Goldbach <jakob.goldbach@gmail.com>
2021-12-18 16:32:09 +0100

    updates

--- find_images.py
+++ find_images.py
@@ -5,34 +5,29 @@ import re
 import requests
 
 
-#%% 
+# %%
+
+# åbn en fil og lad beautifulsoup læse den og forstå html strukturen
 fh = open('src/jean-michel-basquiat-paintings.html')
 page = BeautifulSoup(fh, features="html.parser")
 
+# gem alle image tags i en liste
 imgs = list(page.findAll('img'))
-print(len(imgs))
 
-#%%
-
-family = ['mor', 'far', 'kat', 'sof']
-print(len(family))
-for member in family:
-    print(member.upper())
-
-#%%
-print(imgs[0])
-
-#%% 
-urls = []
-for img in imgs:
-    src = img['src']
-    if src.startswith('http'):
-        if re.search('basq', src, re.IGNORECASE):
-            print(src)
-            urls.append(src)
 
 # %%
-urls[0].split('/')[-1]
+urls = []  # en tom liste
+for img in imgs:  # loop over alle image tags
+
+    # tag src attributen ud af tag'et
+    src = img['src']
+    if src.startswith('http'):
+
+        # hvis 'basq' er i src'en ...
+        if re.search('basq', src, re.IGNORECASE):
+            # tilføj til listen
+            urls.append(src)
+
 
 # %%
 for url in urls:
@@ -41,13 +36,13 @@ for url in urls:
     r = requests.get(url, allow_redirects=False)
     # split på / og tag sidste komponent
     fname = url.split('/')[-1]
-    # overskriv fname med folder navn+filnavn
+    # overskriv fname med foldernavn/filnavn
     fname = f'images/{fname}'
-    print(fname)
 
+    # lav ny fil ('w') som skal have binært data ('b')
     fh = open(fname, 'wb')
+    # save downloaded content i filen
     fh.write(r.content)
 
+    # luk filen (fortæller operativ systemet at du er færdig)
     fh.close()
-
-# %%
---
+++ sandbox.py
@@ -0,0 +1,8 @@
+
+#%% 
+family = ['mor', 'far', 'kat', 'sof']  # index 0,1,2,3
+print(len(family))
+print(family[2])   # kat
+
+for member in family:
+    print(member.upper())  # MOR (ny linie) FAR ...
